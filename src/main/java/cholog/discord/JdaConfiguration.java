f43cfe47185565f886ac9df99a06c04ad55a2aab
jaeyeonling <jaeyeonling@gmail.com>
2024-07-10 13:54:28 +0900

    enable intents

--- src/main/java/cholog/discord/JdaConfiguration.java
+++ src/main/java/cholog/discord/JdaConfiguration.java
@@ -5,12 +5,14 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.EnumSet;
 import java.util.List;
 
 @Configuration
@@ -33,8 +35,19 @@ class JdaConfiguration {
 
     @Bean
     JDA jda() throws InterruptedException {
+        final var intents = EnumSet.of(
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_PRESENCES,
+                GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+                GatewayIntent.DIRECT_MESSAGES,
+                GatewayIntent.MESSAGE_CONTENT
+        );
         final var jda = JDABuilder.createDefault(jdaProperties.token())
                 .setStatus(OnlineStatus.ONLINE)
+                .enableIntents(intents)
                 .addEventListeners(eventListeners.toArray())
                 .build()
                 .awaitReady();
