f81c913243faf2c5818ea2bcccbb0ec2e5d89dde
jaeyeonling <jaeyeonling@gmail.com>
2024-07-10 12:22:45 +0900

    change merge method from enum to string

--- src/main/java/cholog/github/PullRequestMergeRequest.java
+++ src/main/java/cholog/github/PullRequestMergeRequest.java
@@ -4,8 +4,17 @@ public record PullRequestMergeRequest(
         String commitTitle,
         String commitMessage,
         String sha,
-        MergeMethod mergeMethod
+        String mergeMethod
 ) {
+
+    public PullRequestMergeRequest(
+            final String commitTitle,
+            final String commitMessage,
+            final String sha,
+            final MergeMethod mergeMethod
+    ) {
+        this(commitTitle, commitMessage, sha, mergeMethod.name());
+    }
 
     public enum MergeMethod {
         MERGE,
